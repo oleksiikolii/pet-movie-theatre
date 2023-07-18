4a35f6017a4691db2dcff50a1f1c81ffbc0edb08
Oleksii Kolii <igravkompik@gmail.com>
2023-07-18 18:09:19 +0300

    add models

---
+++ cinema/models.py
@@ -0,0 +1,164 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.db.models import UniqueConstraint
+
+
+class Guest(AbstractUser):
+    pass
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(
+        max_length=255
+    )
+    last_name = models.CharField(
+        max_length=255
+    )
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Producer(models.Model):
+    first_name = models.CharField(
+        max_length=255
+    )
+    last_name = models.CharField(
+        max_length=255
+    )
+
+
+class Movie(models.Model):
+    title = models.CharField(
+        max_length=255
+    )
+    release_date = models.DateField()
+    rating = models.IntegerField()
+    country = models.CharField(
+        max_length=255
+    )
+    producers = models.ManyToManyField(
+        Producer,
+        related_name="movies"
+    )
+    actors = models.ManyToManyField(
+        Actor,
+        related_name="movies"
+    )
+    genres = models.ManyToManyField(
+        Genre,
+        related_name="movies"
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MovieSession(models.Model):
+    movie = models.ForeignKey(
+        Movie,
+        related_name="sessions",
+        on_delete=models.CASCADE
+    )
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        related_name="sessions",
+        on_delete=models.CASCADE
+    )
+    show_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    guest = models.ForeignKey(
+        get_user_model(),
+        related_name="orders",
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return str(self.created_at)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    order = models.ForeignKey(
+        Order,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
+    movie_session = models.ForeignKey(
+        MovieSession,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["movie_session", "seat", "row"],
+                             name="unique_tickets")
+        ]
+
+    def __str__(self) -> str:
+        return (f"{self.movie_session.movie.title} "
+                f"{self.movie_session.show_time} "
+                f"(row: {self.row}, seat: {self.seat})")
+
+    def clean(self) -> None:
+        if not 1 <= self.row <= self.movie_session.cinema_hall.rows:
+            raise ValidationError({
+                "row": "row number must be in available range:"
+                       " (1, rows): "
+                       f"(1, {self.movie_session.cinema_hall.rows})"
+            })
+
+        if not 1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row:
+            raise ValidationError({
+                "seat": "seat number must be in available range:"
+                        " (1, seats_in_row):"
+                        f" (1, {self.movie_session.cinema_hall.seats_in_row})"
+            })
+
+    def save(self,
+             force_insert: bool = False,
+             force_update: bool = False,
+             using: bool = None,
+             update_fields: bool = None) -> callable:
+        self.full_clean()
+        return super(Ticket, self).save(force_insert,
+                                        force_update,
+                                        using,
+                                        update_fields)
