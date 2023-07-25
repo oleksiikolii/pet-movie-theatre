99125b66ff33886c256fdcce850e4d52519f197b
Oleksii Kolii <igravkompik@gmail.com>
2023-07-25 16:32:44 +0300

    refactored debug getter

--- movie_theatre/settings.py
+++ movie_theatre/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get("DJANDO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1"
